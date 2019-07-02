d15001f18f133b2b88f4ce241f37ec0000623d4d
Ruslan Salikhov <ruslan.salihov@gmail.com>
2019-07-02 13:50:55 +0500

    straightforward solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    let opened = [];
+    let closed = {};
+
+    bracketsConfig.forEach(v => {
+        opened.push(v[0]);
+        closed[v[1]] = v[0];
+    });
+
+    for (let i = 0; i < str.length; i++) {
+        let bracket = str[i];
+
+        if (opened.indexOf(bracket) >= 0) {
+            // edge case
+            if (closed.hasOwnProperty(bracket)
+                && closed[bracket] === bracket
+                && stack.slice(-1)[0] === bracket) {
+
+                stack.pop();
+                    
+            } else {
+                stack.push(bracket);
+            }
+
+        } else if (closed.hasOwnProperty(bracket)) {
+            let check = stack.pop();
+            if (check !== closed[bracket])
+                return false;
+        }
+    }
+    return stack.length === 0;
 }
